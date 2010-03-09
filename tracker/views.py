dd5e4ab0a627650a669175b2424a14f0e8a96e01
Zeebo <leterip@gmail.com>
2010-03-09 17:01:17 -0500

    added basic parsing mechanism

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.utils.encoding import smart_str
-from rain.tracker.models import Torrent
+from rain.tracker.models import Torrent, Peer
 from rain import utils
 def tracker_error_response(value):
   return HttpResponse(utils.bencode({'failure reason':value}), mimetype='text/plain')
@@ -13,5 +13,15 @@ def announce(request):
     torrent = Torrent.objects.filter(info_hash=info_hash).get()
   except Torrent.MultipleObjectsReturned, Torrent.DoesNotExist:
     return tracker_error_response('Problem with info_hash')
+  
+  #Parse data out
+  peer_id = request.GET.get('peer_id', 'no-peer-id')
+  port = request.GET.get('port', 0)
+  event = request.GET.get('event', 'update')
+  
+  
+  #Make a new peer object based on the announce
+  #new_peer = Peer()
+  
   return tracker_error_response('found torrent. peerlist NYI')
 
