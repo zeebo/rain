5d91a12e6338672831f0eff0c1720c743671c519
Zeebo <leterip@gmail.com>
2010-03-09 14:58:12 -0500

    added basic checking to see if torrent is known

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,17 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.utils.encoding import smart_str
+from rain.tracker.models import Torrent
 from rain import utils
+def tracker_error_response(value):
+  return HttpResponse(utils.bencode({'failure reason':value}), mimetype='text/plain')
+  
 def announce(request):
-  return HttpResponse(utils.bencode({'failure-reason':'owned'}), mimetype='text/plain')
+  request.encoding = 'iso-8859-1'
+  info_hash = request.GET.get('info_hash', '').encode('iso-8859-1').encode('hex')
+  try:
+    torrent = Torrent.objects.filter(info_hash=info_hash).get()
+  except Torrent.MultipleObjectsReturned, Torrent.DoesNotExist:
+    return tracker_error_response('Problem with info_hash')
+  return tracker_error_response('found torrent. peerlist NYI')
 
